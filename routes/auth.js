e0fa2f6338ef0e0c4dfe76864790d90c691de9f4
TejramChafle <tejram_chafle@yahoo.in>
2020-09-20 16:24:27 +0530

    Changed nodemailer credentials

--- routes/auth.js
+++ routes/auth.js
@@ -131,12 +131,27 @@ router.post("/signup", async (req, resp) => {
 
 // Send Mail function using Nodemailer 
 function sendMail(user) {
-    let mailTransporter = nodemailer.createTransport({
+    /* let mailTransporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
             user: "info@wizbee.co.in",
             pass: "working@24"
         }
+    }); */
+
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let mailTransporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+        },
     });
 
     // Setting credentials 
